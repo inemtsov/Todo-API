4c9b064010a3aa4d39ac07178c37f7ed3a5c7f23
Ilya Nemtsov <nemtsovilya@yahoo.com>
2017-01-19 16:39:55 -0800

    convert GET /todos to sequelize

--- server.js
+++ server.js
@@ -13,25 +13,26 @@ app.get('/', function(req, res) {
 });
 
 app.get('/todos', function(req, res) {
-	var queryParam = req.query;
-	var filteredTodos = todos;
+	var query = req.query;
+	var where = {};
 
-	if (queryParam.hasOwnProperty('completed') && queryParam.completed === 'true') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: true
-		});
-	} else if (queryParam.hasOwnProperty('completed') && queryParam.completed === 'false') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: false
-		});
-	}
+if (query.hasOwnProperty('completed') && query.completed === 'true') {
+	where.completed = true;
+} else if(query.hasOwnProperty('completed') && query.completed === 'false'){
+	where.completed = false;
+}
 
-	if (queryParam.hasOwnProperty('q') && queryParam.q.length > 0) {
-		filteredTodos = _.filter(filteredTodos, function(todo) {
-			return todo.description.toLowerCase().indexOf(queryParam.q.toLowerCase()) > -1;
-		});
-	}
-	res.json(filteredTodos);
+if (query.hasOwnProperty('q') && query.q.length > 0) {
+	where.description = {
+		$like: '%' + query.q + '%'
+	};
+}
+
+	db.todo.findAll({where: where}).then(function(todos){
+		res.json(todos);	
+	}, function(e){
+		res.status(500).send();
+	});
 });
 
 app.get('/todos/:id', function(req, res) {
